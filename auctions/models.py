41042e3d0faddebb05c8e6c7d40296c35a6093b8
Andrés Elizalde <andres@MacBook-Pro-de-Andres.local>
2021-07-19 17:39:22 +0200

    added listing page functionality

--- auctions/models.py
+++ auctions/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models.base import Model
@@ -6,7 +7,7 @@ from django.db.models.fields.related import OneToOneField
 
 
 class User(AbstractUser):
-    pass
+    watchlist = models.ManyToManyField('AuctionListing', blank=True, related_name="users_in_watchlist")
 
 
 class Category(models.Model):
@@ -22,14 +23,16 @@ class AuctionListing(models.Model):
     title = CharField(max_length=64)
     description = TextField()
     category = models.ForeignKey(Category, on_delete=models.PROTECT, blank=True, null=True, related_name="listings")
-    image_url = URLField(blank=True)
+    image_url = URLField(blank=True, null=True, default="../static/auctions/no-image-available.jpg")
     starting_bid_value = DecimalField(max_digits=6, decimal_places=2)
-    winner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, editable=False, default=None ,related_name="wins")
     is_active = BooleanField(default=True)
-    users_in_watchlist = models.ManyToManyField(User, blank=True, related_name="watchlist")
 
     def __str__(self) -> str:
         return f"{self.title} ({self.user})"
+    
+    @property
+    def current_bid(self):
+        return self.bids.order_by("-value").first()
 
 
 class Bid(models.Model):
@@ -39,7 +42,7 @@ class Bid(models.Model):
     listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE, related_name="bids")
 
     def __str__(self) -> str:
-        return f"{self.value}"
+        return f"[{self.listing.title}] {self.value} ({self.user})"
 
 class Comment(models.Model):
     timestamp = DateTimeField(auto_now_add=True, editable=False)
--- auctions/views.py
+++ auctions/views.py
@@ -8,7 +8,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import Category, User, AuctionListing
+from .models import Category, User, AuctionListing, Bid, Comment
 
 class NewListingForm(forms.Form):
     title = forms.CharField(max_length=64, label="Title")
@@ -81,22 +81,19 @@ def create_listing(request):
     if request.method == "POST":
         form = NewListingForm(request.POST)
         if form.is_valid():
-            title = form.cleaned_data["title"]
-            description = form.cleaned_data["description"]
-            starting_bid_value = form.cleaned_data["starting_bid_value"]
-            image_url = form.cleaned_data["image_url"]
-            category = form.cleaned_data["category"]
             try:
                 if request.user.is_authenticated:
                     user = request.user
                     listing = AuctionListing(
                         user = user, 
-                        title = title, 
-                        description = description,
-                        category = category,
-                        image_url = image_url,
-                        starting_bid_value = starting_bid_value
+                        title = form.cleaned_data["title"], 
+                        description = form.cleaned_data["description"],
+                        category = form.cleaned_data["category"],
+                        starting_bid_value = form.cleaned_data["starting_bid_value"]
                         )
+                    image_url = form.cleaned_data["image_url"]
+                    if (image_url):
+                        listing.image_url = image_url 
                     listing.save()
                     return HttpResponseRedirect(reverse("index"))
             except:
@@ -113,3 +110,75 @@ def create_listing(request):
         return render(request, "auctions/new_listing.html", {
             "form": NewListingForm()
         })
+
+
+def listing(request, id):
+    if request.method == "POST":
+        if True:
+            listing = AuctionListing.objects.get(pk=id)
+            if request.POST.get("bid"):
+                bid_value = float(request.POST.get("bid"))
+                if listing.current_bid:
+                    current_bid = listing.current_bid.value
+                else:
+                    current_bid = listing.starting_bid_value
+                if request.user.is_authenticated:
+                    user = request.user
+                    if listing.user == user:
+                        return render(request, "auctions/listing.html", {
+                            "message": "You can't place a bid on your own listing",
+                            "listing": listing
+                        })
+                    elif bid_value <= current_bid:
+                        return render(request, "auctions/listing.html", {
+                            "message": f"The bid must be greater than { current_bid }",
+                            "listing": listing
+                        })
+                    else: 
+                        bid = Bid(user = user, 
+                                value = bid_value, 
+                                listing = listing)
+                        bid.save()
+                else: print("No estas logueado")
+            print(request.POST)
+            if request.user.is_authenticated:
+                user = request.user
+                if request.POST.get("watchlist"):
+                    watchlist = user.watchlist.filter(pk=id)
+                    if watchlist:
+                        user.watchlist.remove(listing)
+                    else:
+                        user.watchlist.add(listing)
+                    return render(request, "auctions/listing.html", {
+                        "listing": listing
+                    })
+                elif request.POST.get("close"):
+                    listing.is_active = False
+                    listing.save()
+                    return render(request, "auctions/listing.html", {
+                            "message": "Your auction was closed successfuly...",
+                            "listing": listing
+                        })
+                elif request.POST.get("comment"):
+                    comment = Comment(user = user,
+                                comment = request.POST.get("comment"),
+                                listing = listing)
+                    comment.save()
+                    return render(request, "auctions/listing.html", {
+                            "listing": listing
+                        })
+            else: 
+                return render(request, "auctions/listing.html", {
+                        "message": "Your bid was successful...",
+                        "listing": listing
+                    })
+        else:
+            print(" FIN EXCEPTERRORRR")
+    else:
+        try:
+            listing = AuctionListing.objects.get(pk=id)
+            return render(request, "auctions/listing.html", {
+                    "listing": listing
+                })
+        except:
+            print("404")
